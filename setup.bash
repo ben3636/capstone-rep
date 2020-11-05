6a79125eaf253ac56c765a444037371acbf6d917
Ben Nichols <33106643+ben3636@users.noreply.github.com>
2020-11-05 10:37:53 -0500

    Fixed non-root install

--- setup.bash
+++ setup.bash
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Initializing, Updating, and Installing Components"
 apt-get update -y && apt install metasploit-framework -y &&  apt install nmap -y && \
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && service ssh start && msfupdate && apt-get -y install openvas && gvm-setup
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && service ssh start && msfupdate && sudo apt-get -y install openvas && sudo gvm-setup
